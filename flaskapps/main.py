5df1a5de94fd22ccba7427d52b574a0dee7c0b7d
ewilyna <161340295+ewilyna@users.noreply.github.com>
2024-12-22 17:02:06 +0300

    update yml

--- flaskapps/main.py
+++ flaskapps/main.py
@@ -6,4 +6,4 @@ app = Flask(__name__)
 def hello():
     return "<html><head></head> <body> Hello World! </body></html>"
 if __name__ == "__main__":
-    app.run(host='127.0.0.1',port=5000)
+    app.run(host='127.0.0.1',port=1337)
--- flaskapps/st.sh
+++ flaskapps/st.sh
@@ -1,5 +1,5 @@
 export PATH=$HOME/.local/bin:$PATH
-gunicorn --bind 127.0.0.1:5000 wsgi:app & APP_PID=$!
+gunicorn --bind 127.0.0.1:1337 wsgi:app & APP_PID=$!
 sleep 5
 echo $APP_PID
 kill -TERM $APP_PID
