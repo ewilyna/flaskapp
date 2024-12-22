c6c11c41070c9761e570f6a1a91f48add49d6982
ewilyna <161340295+ewilyna@users.noreply.github.com>
2024-12-22 15:11:24 +0300

    update yml

--- flaskapps/st.sh
+++ flaskapps/st.sh
@@ -9,14 +9,11 @@ APP_PID=$!
 sleep 5
 
 if ! ps -p $APP_PID > /dev/null; then
-  echo "Ошибка: Gunicorn не запустился."
-  exit 1
 fi
 
 # Проверяем наличие ngrok и запускаем его
 if [ ! -f ./ngrok ]; then
   echo "Ошибка: ngrok не найден."
-  exit 1
 fi
 
 chmod +x ./ngrok
@@ -26,13 +23,11 @@ sleep 5
 
 if ! ps -p $NGROK_PID > /dev/null; then
   echo "Ошибка: ngrok не запустился."
-  exit 1
 fi
 
 # Получаем URL ngrok
 if ! command -v jq &> /dev/null; then
   echo "Ошибка: jq не установлен."
-  exit 1
 fi
 
 NGROK_URL=$(curl --silent http://127.0.0.1:4040/api/tunnels | jq -r '.tunnels[0].public_url')
