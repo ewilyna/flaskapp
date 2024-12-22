87769e40a9530d105a518cddd1c65e7cca167f08
ewilyna <161340295+ewilyna@users.noreply.github.com>
2024-12-22 14:37:26 +0300

    update yml

--- flaskapps/st.sh
+++ flaskapps/st.sh
@@ -1,6 +1,25 @@
-gunicorn --bind 0.0.0.0:5000 wsgi:app & APP_PID=$!
+#!/bin/bash
+
+# Запускаем Gunicorn для Flask-приложения
+gunicorn --bind 0.0.0.0:5000 wsgi:app &  # Поднятие приложения на порту 5000
+APP_PID=$!                               # Сохраняем PID приложения
+
+# Даём приложению немного времени для запуска
 sleep 5
-echo $APP_PID
-kill -TERM $APP_PID
-echo process gunicorns kills
-exit 0
+
+# Запускаем ngrok для проброса порта 5000
+./ngrok http 5000 > /dev/null &         # Запуск ngrok в фоне
+NGROK_PID=$!
+
+# Даём ngrok время для получения публичного URL
+sleep 5
+
+# Выводим публичный URL для доступа к приложению
+curl --silent http://127.0.0.1:4040/api/tunnels | jq -r '.tunnels[0].public_url'
+
+# Ожидаем завершения работы (опционально)
+wait $APP_PID
+
+# Очищаем ресурсы
+kill -TERM $NGROK_PID
+echo "Gunicorn и ngrok завершены."
